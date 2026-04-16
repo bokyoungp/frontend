10a08d33baaa3a00c5c583bb48f4b25d6559c736
bokyoungp <bokyoungp@gmail.com>
2026-04-16 16:59:32 +0900

    js

---
+++ techstory/js/fuction_out_over.js
@@ -0,0 +1,8 @@
+function out(card) {
+  // let card = document.getElementById("card");
+  card.src = "image/card_back.png";
+}
+function over(card) {
+  // let card = document.getElementById("card");
+  card.src = "image/card_front.png";
+}
